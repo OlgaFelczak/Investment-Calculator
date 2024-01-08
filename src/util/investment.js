911badc13f02f3f568c63c4fafad9e958ad97e23
Olga Felczak <ofelczak@gmail.com>
2024-01-08 18:36:27 +0000

    Update currency formatter to use en-GB locale

--- src/util/investment.js
+++ src/util/investment.js
@@ -30,7 +30,7 @@ export function calculateInvestmentResults({
 // The browser-provided Intl API is used to prepare a formatter object
 // This object offers a "format()" method that can be used to format numbers as currency
 // Example Usage: formatter.format(1000) => yields "$1,000"
-export const formatter = new Intl.NumberFormat('en-UK', {
+export const formatter = new Intl.NumberFormat('en-GB', {
   style: 'currency',
   currency: 'GBP',
   minimumFractionDigits: 0,
